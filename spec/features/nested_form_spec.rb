d59cb110a183433245165901879bfd75964c7edd
Gordon Gao <gordonsdev@gmail.com>
2016-08-02 18:01:14 +0000

    Done.

--- spec/features/nested_form_spec.rb
+++ spec/features/nested_form_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'nested form for tag in post', :type => :feature do 
+describe 'nested form for tag in post', :type => :feature do
   it 'can create a post without a new tag' do
     visit 'posts/new'
     within(all('.field').first) do
@@ -11,13 +11,15 @@ describe 'nested form for tag in post', :type => :feature do
     expect(page).to have_content(@post1.name)
   end
 
-  it 'can create a post with a new tag' do 
+  it 'can create a post with a new tag' do
     visit 'posts/new'
     within(all('.field').first) do
+      # binding.pry
       fill_in('Name', :with => @post1.name)
       fill_in('Content', :with => @post1.content)
     end
     within(all('.field').last) do
+
       fill_in('Name', :with => "witty")
     end
     click_button('Create Post')
