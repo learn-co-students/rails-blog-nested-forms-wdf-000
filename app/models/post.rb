240e2068f2fdd1865d7991f19f1a40feb4760ae1
satub <satu.barnhill@gmail.com>
2016-09-06 15:38:51 -0400

    Done with wonder

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,11 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+
+  accepts_nested_attributes_for :tags, reject_if: :empty_tagname?
+
+
+    def empty_tagname?(tag_attr)
+      tag_attr[:name].nil? ? true : tag_attr[:name].empty? 
+    end
 end
