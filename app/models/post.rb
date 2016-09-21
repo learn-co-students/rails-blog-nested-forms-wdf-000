7ace00b3868a2f803a963c96d8870a134ccea18a
Flatiron School <github email address>
2016-09-21 14:47:28 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,23 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+
+  # accepts_nested_attributes_for :tags
+
+  accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
+
+
+    def tags_attributes=(tag_attributes)
+      # tag_attributes.values.each do |tag_attribute|
+        tag_attributes.each do |index, tag_attribute|
+        # if tag_attribute[:name].present?
+          tag = Tag.find_or_create_by(tag_attribute)
+            self.post_tags.build(:tag => tag)
+          # ^^^ instantiating an instance of the join model and putting the tag in
+          # self.tag << tag -- inefficient
+          # end
+        # end
+      end
+    end
+
 end
