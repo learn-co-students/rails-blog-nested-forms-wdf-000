68089cb8347de9fc6a671fc8d81ae71daaf6b25b
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-10-03 17:01:39 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: proc {|attributes| attributes['name'].blank?}
+
 
   validates_presence_of :name, :content
+
+  # def tags_attributes=(tag_attributes)
+  #     tag_attributes.values.each do |tag_attribute|
+  #       tag = Tag.find_or_create_by(tag_attribute)
+  #       self.tags << tag
+  #       end
+  #   else
+  #
+  # end
 end
